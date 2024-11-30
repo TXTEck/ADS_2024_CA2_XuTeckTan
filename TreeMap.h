21185c50cf8d6d23e29cab8d26ac975d392ee97e
TXTEck <91459117+TXTEck@users.noreply.github.com>
2024-12-01 04:31:44 +0800

    []operator method

--- TreeMap.h
+++ TreeMap.h
@@ -90,11 +90,11 @@ int TreeMap<K, V>::size() const {
 
 //Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
 template <typename K, typename V>
-V& TreeMap<K, V>::operator[](const K& key) {
-    if (!containsKey(key)) {
-        put(key, V());
+V* TreeMap<K, V>::operator[](const K& key) {
+    if (containsKey(key)) {
+        return &get(key); 
     }
-    return get(key);
+    return nullptr; 
 }
 
 
